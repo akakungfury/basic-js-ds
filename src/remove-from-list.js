f672e30d46b03fd9290b279fd196ef472ff1c08a
Aliaksandr Khudnitski <alekskhudnitskiy@gmail.com>
2023-04-17 15:17:38 +0300

    feat: implement stak and remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  while(l.value === k) {
+    l = l.next;
+  }
+
+  const head = l;
+
+  while(l.next !== null) {
+    l.next.value === k ? l.next = l.next.next : l = l.next
+  }
+
+  return head;
 }
 
 module.exports = {
--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+  recalculateLength() {
+    this.length =  this.items.length;
+  }
+
+  push(element) {
+    this.items.push(element);
+    this.recalculateLength();
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const value = this.items.pop();
+    this.recalculateLength();
+    return value;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.length ? this.items[this.length - 1] : undefined;
   }
 }
 
